e8bb1b50a5e0a7741523a824783cb70db9968b94
afaucheryved <florian.malard@gmail.com>
2026-06-29 12:17:33 +0200

    The  class still needs to be debugged. The goal is to maintain its state across client requests and utilize the methods it has inherited for this purpose. Therefore, an additional route needs to be created for each relevant method of .

--- app/domain/initalize_instances.py
+++ app/domain/initalize_instances.py
@@ -1,5 +1,11 @@
 from app.domain.spliceia_calculation import SpliceAIModels
+from app.schemas.general_schema import InternalGeneticVariant
 
 my_model = SpliceAIModels()
 
 print("\ninitialisation my_model completed !\n")
+
+my_internal_genetic_variant = InternalGeneticVariant()
+
+print("initialisation my_internal_genetic_variant completed !\n")
+
